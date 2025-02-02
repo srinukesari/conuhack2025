17e0f848123064fbe295e040083b894edb732c05
srinukesari <srinukesari333@gmail.com>
2025-02-02 02:44:26 -0500

    added yarn and resources availability in UI

--- src/code/Dashboard.js
+++ src/code/Dashboard.js
@@ -1,123 +1,159 @@
 import React, { useState, useEffect, use } from "react";
+import { motion } from "framer-motion";
 import BarChart from "./BarChart";
 import Card from "./Card";
 import MapWidget from "./MapWidget";
 import "./Dashboard.css";
-import { Zoom } from "react-reveal";
 
 const Dashboard = () => {
   const [csvData, setCsvData] = useState([]);
   const [severity, setSeverity] = useState([]);
-  const [onLoad, setOnLoad] = useState(false);
-  const [fireAddressed, setFireAddressed] = useState(0);
-  const [delayed, setDelayed] = useState(0);
   const [damageCost, setDamageCost] = useState(0);
   const [operationCost, setOperationCost] = useState(0);
+  const [fireAddressed, setFireAddressed] = useState(0);
+  const [delayed, setDelayed] = useState(0);
+
+  const [fireEngines, setFireEngines] = useState(0);
+  const [smokeJumpers, setSmokeJumpers] = useState(0);
+  const [tankerPlanes, setTankerPlanes] = useState(0);
+  const [helicopters, setHelicopters] = useState(0);
+  const [groundCrews, setGroundCrews] = useState(0);
+
+  const [onLoad, setOnLoad] = useState(false);
+  const [dummyState, setDummyState] = useState(false); // Dummy state variable
 
   useEffect(() => {
-    // Generate 10 random coordinates with labels
-    // const labels = ["Low", "Medium", "High"];
-    // const randomCoordinates = Array.from({ length: 10 }, () => [
-    //   (Math.random() * 180 - 90).toFixed(6), // Latitude between -90 and 90
-    //   (Math.random() * 360 - 180).toFixed(6), // Longitude between -180 and 180
-    //   labels[Math.floor(Math.random() * labels.length)], // Random label
-    // ]);
-    // setCsvData(randomCoordinates);
+    fetch("http://localhost:11000/get_resources", {
+      method: "GET",
+      headers: {
+        "Content-Type": "text/csv",
+      },
+    }).then(async (response) => {
+      const responseData = await response.json();
+      console.log("Response Data:", responseData);
+      setHelicopters(responseData.data?.Available_resources?.Helicopters);
+      setSmokeJumpers(responseData.data?.Available_resources?.Smoke_Jumpers);
+      setTankerPlanes(responseData.data?.Available_resources?.Tanker_Planes);
+      setFireEngines(responseData.data?.Available_resources?.Fire_Engines);
+      setGroundCrews(responseData.data?.Available_resources?.Ground_Crews);
+    });
   }, []);
 
-  const handleFileUpload = (event) => {
+  const readCSV = async (file) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const text = e.target.result;
+        const rows = text.split("\n").map((row) => row.split(","));
+        resolve(rows);
+      };
+      reader.onerror = (error) => reject(error);
+      reader.readAsText(file);
+    });
+  };
+
+  const handleFileUpload = async (event) => {
     const file = event.target.files[0];
-    const reader = new FileReader();
+    try {
+      const rows = await readCSV(file);
+      console.log("CSV Data:", rows);
 
-    reader.onload = async (e) => {
-      const text = e.target.result;
-      const rows = text.split("\n").map((row) => row.split(","));
-      // setCsvData(rows);
-      try {
-        const response = await fetch("http://localhost:11000/process_csv", {
-          method: "POST",
-          headers: {
-            "Content-Type": "text/csv",
-          },
-          body: text,
-        });
-        console.log("response", text.split("\n"));
+      // Send POST request to /csv endpoint
+      const response = await fetch("http://localhost:11000/process_csv", {
+        method: "POST",
+        headers: {
+          "Content-Type": "text/csv",
+        },
+        body: rows.map((row) => row.join(",")).join("\n"),
+      });
 
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
 
-        const responseData = await response.json();
-        const labels = Object.values(responseData.data?.Fire_severity_report);
-        setFireAddressed(responseData.data?.Number_of_fires_addressed);
-        setDelayed(responseData.data?.Number_of_fires_delayed);
-        setDamageCost(
-          responseData.data?.Estimated_damage_costs_from_delayed_responses
-        );
-        setOperationCost(responseData.data?.Total_operational_costs);
-        setSeverity(labels);
+      const responseData = await response.json();
+      const labels = Object.values(responseData.data?.Fire_severity_report);
+      setFireAddressed(responseData.data?.Number_of_fires_addressed);
+      setDelayed(responseData.data?.Number_of_fires_delayed);
+      setDamageCost(
+        responseData.data?.Estimated_damage_costs_from_delayed_responses
+      );
+      setOperationCost(responseData.data?.Total_operational_costs);
+      setSeverity(labels);
 
-        const coordinates = [];
-        const { Coordinates } = responseData.data;
-        Object.entries(Coordinates).forEach(([severity, severityGroup]) => {
-          Object.entries(severityGroup).forEach(([level, coords]) => {
-            coords.forEach((coord) => {
-              coordinates.push([...coord, level]);
-            });
+      const coordinates = [];
+      const { Coordinates } = responseData.data;
+      Object.entries(Coordinates).forEach(([severity, severityGroup]) => {
+        Object.entries(severityGroup).forEach(([level, coords]) => {
+          coords.forEach((coord) => {
+            coordinates.push([...coord, level]);
           });
         });
+      });
 
-        console.log("coordinates", coordinates);
-        setCsvData(coordinates);
-        setOnLoad(true);
-      } catch (error) {
-        console.error("Error:", error);
-      }
-    };
-
-    reader.readAsText(file);
+      console.log("coordinates", coordinates);
+      setCsvData(coordinates);
+      setOnLoad(true);
+      setDummyState(!dummyState); // Update dummy state to force re-render
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
+
+  useEffect(() => {
+    // Re-render the page when severity changes
+    console.log("Severity changed:", severity);
+  }, [severity]);
 
   return (
     <div className="dashboard">
+      <header className="dashboard-header">
+        <h1>Whiplash Dashboard</h1>
+        <p>
+          Welcome to the admin dashboard. Here you can monitor and manage fire
+          severity data.
+        </p>
+      </header>
       <main className="dashboard-main">
-        <h2>Hello, Admin</h2>
-        <label className="trending-button">
-          Upload CSV
-          <input
-            type="file"
-            accept=".csv"
-            onChange={handleFileUpload}
-            className="file-input"
-          />
-        </label>
-        {onLoad && (
-          <div className="content">
-            <Zoom>
-              <div className="cards">
-                <Card title="Total Operation Cost" value={operationCost} />
-
-                <Card title="No of Fires Addressed" value={fireAddressed} />
-
-                <Card title="No of Delayed" value={delayed} />
-
-                <Card title="Damage Cost" value={damageCost} />
-              </div>
-            </Zoom>
-
-            <div className="chart-and-map">
-              <Zoom>
-                <div className="chart-container">
-                  <BarChart label={severity} />
-                </div>
-              </Zoom>
-              <Zoom>
-                <div className="map-container">
-                  <MapWidget coordinates={csvData} />
-                </div>
-              </Zoom>
-            </div>
+        <div className="upload-and-cards">
+          <label className="trending-button">
+            Upload CSV
+            <input
+              type="file"
+              accept=".csv"
+              onChange={handleFileUpload}
+              className="file-input"
+            />
+          </label>
+          <div className="info-cards">
+            <Card title="Ground Crew" value={groundCrews} />
+            <Card title="Helicopters" value={helicopters} />
+            <Card title="Smoke Jumpers" value={smokeJumpers} />
+            <Card title="Fire Engines" value={fireEngines} />
+            <Card title="Tanker Planes" value={tankerPlanes} />
           </div>
+        </div>
+        {onLoad && (
+          <motion.div
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+            className="content">
+            <div className="cards">
+              <Card title="Total Operation Cost" value={`$${operationCost}`} />
+              <Card title="No of Fires Addressed" value={fireAddressed} />
+              <Card title="No of Delayed" value={delayed} />
+              <Card title="Damage Cost" value={`$${damageCost}`} />
+            </div>
+            <div className="chart-and-map">
+              <div className="chart-container">
+                <BarChart chartData={severity} />
+              </div>
+              <div className="map-container">
+                <MapWidget coordinates={csvData} />
+              </div>
+            </div>
+          </motion.div>
         )}
       </main>
     </div>
